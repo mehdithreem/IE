5bcae628ac5c674ab1c049d525df4bac15431667
mehdithreem <mehdithreem@gmail.com>
2016-06-21 22:28:23 +0430

    JUnit4 added

---
+++ customs/src/ir/customs/test/RuleRelatedLicenseTest.java
@@ -0,0 +1,14 @@
+package ir.customs.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RuleRelatedLicenseTest {
+
+	@Test
+	public void testGetRelatedLicenses() {
+		fail("Not yet implemented");
+	}
+
+}
