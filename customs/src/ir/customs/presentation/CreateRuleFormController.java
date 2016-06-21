92dd186d410759aeb3ed51838ddf7d67cd8af566
mehdithreem <mehdithreem@gmail.com>
2016-06-21 22:16:02 +0430

    a little fix :[]

--- customs/src/ir/customs/presentation/CreateRuleFormController.java
+++ customs/src/ir/customs/presentation/CreateRuleFormController.java
@@ -15,7 +15,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
